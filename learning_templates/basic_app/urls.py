50c9fb18ad978bad435b4473650176684b733b71
ForeignVisitor <souhailkaram11@gmail.com>
2021-01-25 20:45:19 +0100

    first commit

---
+++ learning_templates/basic_app/urls.py
@@ -0,0 +1,10 @@
+from . import views
+from django.urls import path
+
+# Template tagging
+app_name = 'basic_app'
+
+urlpatterns=[
+    path('relative/',views.relative,name='relative'),
+    path('other/',views.other,name='other'),
+]
