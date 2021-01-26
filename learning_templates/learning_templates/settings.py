599fca4df9ed3105c911305f164efbe3a20367fd
ForeignVisitor <77814484+ForeignVisitor@users.noreply.github.com>
2021-01-26 01:49:02 +0100

    Update settings.py

--- learning_templates/learning_templates/settings.py
+++ learning_templates/learning_templates/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '-)!(z1u)^24c&di1ks&lvza(+=*6chf-qv(a%46pvimp@9)l8b'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["foreignvisitor.pythonanywhere.com"]
+ALLOWED_HOSTS = ["foreignvisitor.pythonanywhere.com","localhost", "127.0.0.1"]
 
 
 # Application definition
